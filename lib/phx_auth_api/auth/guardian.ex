d1e7e307ebc73d6b2f1f0dce348f985a65d938ee
rootkc <info@kennethchristensen.dk>
2017-11-09 18:39:41 +0100

    add Guardian callbacks

---
+++ lib/phx_auth_api/auth/guardian.ex
@@ -0,0 +1,19 @@
+defmodule PhxAuthApi.Auth.Guardian do
+  use Guardian, otp_app: :phx_auth_api
+
+  alias PhxAuthApi.Auth
+
+  # return something that identifies the user
+  def subject_for_token(user, _claims) do
+    {:ok, to_string(user.id)}
+  end
+
+  # get resource from jwt token
+  def resource_from_claims(claims) do
+    user = claims["sub"]
+    |> Auth.get_user!
+    {:ok, user}
+    # If something goes wrong here return {:error, reason}
+  end
+
+end
