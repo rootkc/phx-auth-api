33160b78a2a74c5588ffd51cf201675d6aa4e299
rootkc <info@kennethchristensen.dk>
2017-11-14 20:25:22 +0100

    fix typo

--- lib/phx_auth_api/auth/pipeline.ex
+++ lib/phx_auth_api/auth/pipeline.ex
@@ -1,7 +1,7 @@
 defmodule PhxAuthApi.Auth.AuthPipeline do
 
   use Guardian.Plug.Pipeline, otp_app: :phx_auth_api,
-    module: PhxAuthApi.Auth.Guradian,
+    module: PhxAuthApi.Auth.Guardian,
     error_handler: PhxAuthApi.Auth.AuthErrorHandler
 
   plug Guardian.Plug.VerifyHeader, claims: %{"typ" => "access"}, realm: :none
