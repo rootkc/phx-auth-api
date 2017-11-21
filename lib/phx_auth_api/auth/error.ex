2681657fe849456c67df45976c28e066bfbaecef
rootkc <info@kennethchristensen.dk>
2017-11-21 17:56:05 +0100

    add halt to unauthenticated handler

--- lib/phx_auth_api/auth/error.ex
+++ lib/phx_auth_api/auth/error.ex
@@ -5,11 +5,15 @@ defmodule PhxAuthApi.Auth.AuthErrorHandler do
 
   def auth_error(conn, {type, reason}, _opts) do
     body = create_body(to_string(type))
-    send_resp(conn, 401, body)
+    conn
+    |> send_resp(401, body)
+    |> halt
   end
 
   def auth_error(conn) do
     body = create_body("unauthenticated")
-    send_resp(conn, 401, body)
+    conn
+    |> send_resp(401, body)
+    |> halt
   end
 end
