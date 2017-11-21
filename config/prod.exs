5b2f09f99c4dd33f61beeae8349c5fd1a8621c01
rootkc <info@kennethchristensen.dk>
2017-11-21 22:06:31 +0100

    remove cache

--- config/prod.exs
+++ config/prod.exs
@@ -17,7 +17,6 @@ config :phx_auth_api, PhxAuthApiWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "phx-auth-api.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
