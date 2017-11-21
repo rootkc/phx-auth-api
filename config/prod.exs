737a06cb1a2ea741067fe539f4ec9ff855c9e6ab
rootkc <info@kennethchristensen.dk>
2017-11-21 21:23:25 +0100

    update :atom

--- config/prod.exs
+++ config/prod.exs
@@ -24,7 +24,7 @@ config :phx_auth_api, PhxAuthApiWeb.Endpoint,
 config :logger, level: :info
 
 # Configure your database
-config :hello, PhxAuthApiWeb.Repo,
+config :phx_auth_api, PhxAuthApiWeb.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
