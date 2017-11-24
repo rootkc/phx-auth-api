c9f2dd7b134d6f36567d9aa65e56b067c36e8434
rootkc <info@kennethchristensen.dk>
2017-11-24 21:31:23 +0100

    undo test changes

--- test/phx_auth_api/auth/auth_test.exs
+++ test/phx_auth_api/auth/auth_test.exs
@@ -10,18 +10,21 @@ defmodule PhxAuthApi.AuthTest do
     @update_attrs %{password: "some updated password", username: "some updated username"}
     @invalid_attrs %{password: nil, username: nil}
 
-    def user_fixture(attrs) do
-      {:ok, user} = Auth.create_user(attrs)
+    def user_fixture(attrs \\ %{}) do
+      {:ok, user} =
+        attrs
+        |> Enum.into(@valid_attrs)
+        |> Auth.create_user()
       user
     end
 
     test "list_users/0 returns all users" do
-      user = user_fixture(%{password: "some password", username: "some username1"})
+      user = user_fixture()
       assert Auth.list_users() == [user]
     end
 
     test "get_user!/1 returns the user with given id" do
-      user = user_fixture(%{password: "some password", username: "some username2"})
+      user = user_fixture()
       assert Auth.get_user!(user.id) == user
     end
 
@@ -35,26 +38,26 @@ defmodule PhxAuthApi.AuthTest do
     end
 
     test "update_user/2 with valid data updates the user" do
-      user = user_fixture(%{password: "some password", username: "some username3"})
+      user = user_fixture()
       assert {:ok, user} = Auth.update_user(user, @update_attrs)
       assert %User{} = user
       assert user.username == "some updated username"
     end
 
     test "update_user/2 with invalid data returns error changeset" do
-      user = user_fixture(%{password: "some password", username: "some username4"})
+      user = user_fixture()
       assert {:error, %Ecto.Changeset{}} = Auth.update_user(user, @invalid_attrs)
       assert user == Auth.get_user!(user.id)
     end
 
     test "delete_user/1 deletes the user" do
-      user = user_fixture(%{password: "some password", username: "some username5"})
+      user = user_fixture()
       assert {:ok, %User{}} = Auth.delete_user(user)
       assert_raise Ecto.NoResultsError, fn -> Auth.get_user!(user.id) end
     end
 
     test "change_user/1 returns a user changeset" do
-      user = user_fixture(%{password: "some password", username: "some username6"})
+      user = user_fixture()
       assert %Ecto.Changeset{} = Auth.change_user(user)
     end
   end
