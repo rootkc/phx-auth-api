2413bde4444ef7e49071e4288636daae3a140294
rootkc <info@kennethchristensen.dk>
2017-11-25 15:32:21 +0100

    change heroku setup

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+git remote add heroku https://git.heroku.com/phx-auth-api.git
 wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
 sudo mkdir -p /usr/local/lib /usr/local/bin
 sudo tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
@@ -8,9 +9,10 @@ cat > ~/.netrc << EOF
 machine api.heroku.com
   login $HEROKU_LOGIN
   password $HEROKU_API_KEY
+machine git.heroku.com
+  login $HEROKU_LOGIN
+  password $HEROKU_API_KEY
 EOF
 
-cat >> ~/.ssh/config << EOF
-VerifyHostKeyDNS yes
-StrictHostKeyChecking no
-EOF
+# Add heroku.com to the list of known hosts
+ssh-keyscan -H heroku.com >> ~/.ssh/known_hosts
